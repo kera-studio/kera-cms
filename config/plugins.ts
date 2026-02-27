888cfdd83435fd6feecfb35c05ae368571f11789
kera-studio <kerastudio12345@gmail.com>
2026-02-27 20:18:39 +0100

    feat: add provider to upload assets on minio in production env
    fix: production condition

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,33 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
+  env("NODE_ENV") === "production"
+    ? {
+      upload: {
+        config: {
+          provider: "@avorati/strapi-provider-upload-minio",
+          providerOptions: {
+            host:
+              process.env.MINIO_HOST ||
+              process.env.MINIO_ENDPOINT ||
+              "localhost",
+            port: process.env.MINIO_PORT
+              ? parseInt(process.env.MINIO_PORT)
+              : undefined,
+            // If port is not specified, the provider will automatically use the default port based on useSSL (443 for HTTPS, 9000 for HTTP)
+            useSSL: process.env.MINIO_USE_SSL === "true",
+            rejectUnauthorized:
+              process.env.MINIO_REJECT_UNAUTHORIZED !== "false", // default: true (secure)
+            accessKey: process.env.MINIO_ACCESS_KEY,
+            secretKey: process.env.MINIO_SECRET_KEY,
+            bucket: process.env.MINIO_BUCKET || "strapi-uploads",
+            folder: process.env.MINIO_FOLDER || "", // optional
+          },
+        },
+      },
+    }
+    : {};
 
 export default config;
