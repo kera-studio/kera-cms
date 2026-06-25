4cd758ecaf7c0686ce18c7a1cd1c7732b8de068e
kera-studio <kerastudio12345@gmail.com>
2026-06-25 15:16:40 +0200

    feat(employee): add relation to the studio
    chore: update pnpm-lock

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1082,6 +1082,7 @@ export interface ApiEmployeeEmployee extends Struct.CollectionTypeSchema {
           localized: true;
         };
       }>;
+    studio: Schema.Attribute.Relation<'oneToOne', 'api::studio.studio'>;
     Surname: Schema.Attribute.String &
       Schema.Attribute.Required &
       Schema.Attribute.SetPluginOptions<{
